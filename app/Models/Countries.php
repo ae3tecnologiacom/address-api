ae11f2493917951358440e31643262f0ac370076
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-02-24 17:23:15 -0400

    criando model de paises de cidades

---
+++ app/Models/Countries.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Countries extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'public.countries';
+}
